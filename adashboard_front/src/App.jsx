6d4f18ca0dfcd542f3eb8394b90cdc098ed061dd
egainon <miliegainon@hotmail.fr>
2026-01-12 14:18:25 +0100

    map data theme

--- adashboard_front/src/App.jsx
+++ adashboard_front/src/App.jsx
@@ -6,22 +6,33 @@ import './App.css'
 
 function App() {
 
+const [data, setData]= useState([]);
+// const [themes, setThemes]= useState("");
 
 useEffect(() => {
    const loadData = async () => {
     const res = await fetch("http://localhost:3000/themes");
-    const data = await res.json();
-    console.log(data);
+    setData(await res.json());
  } 
  loadData();
 }, []);
 
- 
-
+//phrase de chargement lors du loadData
+ if(!data){
+  return <h1>Chargement ...</h1>
+ }
+ console.log(data[0]);
 
   return (
     <>
-      
+    {data.map((theme) => {
+          return (
+      <div key={theme.id}>
+        <h2>{theme.name}</h2>
+        <p>{theme.skills[0].label}</p>
+        </div>
+    )})
+    }
     </>
   )
 }
